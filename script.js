6226a12c40af23a91479c7147d4cb15bfba81ec8
Prasadk-1 <87906300+Prasadk-1@users.noreply.github.com>
2021-10-21 22:28:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,17 @@
 //1.Odd NO
       let array = [1,2,3,4,5,6,7,8,9,10];
 
-       let getOddNumbers = (function(){
-           let oddNumbers = array.filter(number => number%2 !== 0)
+      (function(arr){
+           let oddNumbers = arr.filter(number => number%2 !== 0)
            console.log(oddNumbers);
-       });
-
-        getOddNumbers();
-          
+       })(array);
 
 
 
 //2.Title caps
 let arr = ["ENgineer", "prasad" , "kasar"];
 
-let titleCase = (function() {
+(function() {
             let lowerArr = arr.map(name => name.toLowerCase());
          for(var i = 0 ; i < lowerArr.length ; i++){
             let newArr = lowerArr[i].charAt(0).toUpperCase()+ lowerArr[i].slice(1);
@@ -29,7 +26,7 @@ let titleCase = (function() {
 //3.sum of numbers in an array
 let arr = [1,2,3,4,5,6,7,8,9,10];
 
- let addArrayNums=(function () {
+ (function () {
 	let total = 0;
 	for (let i in arr) {
       total += arr[i];
@@ -43,7 +40,7 @@ let arr = [1,2,3,4,5,6,7,8,9,10];
 const random = ['madam', 12321, 'abcba','acs'];
 const newArr = [];
 
-const palindrome = (function (abc) {
+(function (abc) {
     
     for(let i=0; i < abc.length; i++) {
         let rev = String(abc[i]).split('').reverse().join('');
@@ -60,7 +57,7 @@ const palindrome = (function (abc) {
 const arr1 = [1,2,3,3,'a',1,'a','b'];
 
 
-const fun = (function(duplicateArray) {
+(function(duplicateArray) {
            let newArr = [...new Set(duplicateArray)];
            console.log(newArr);
 })(arr1);
@@ -70,7 +67,7 @@ const fun = (function(duplicateArray) {
 //6.Rotate an Array K times
       let array = [1,2,3,4,5,6,7,8,9,10];
 
-    const rotate=(function() {
+    (function() {
         return {
             rotate1: function(nums, k) {
         for (let i = 0; i < k; i++) {
